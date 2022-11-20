fa1d2bb2385dd8763235e646e39a6b15eab0f6f7
Igoy <96291722+igoydev@users.noreply.github.com>
2022-11-20 15:42:11 +0700

    Add files via upload
    Final Version 1.0

---
+++ FinalProject_Scheduler/FinalProject_Scheduler/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  FinalProject_Scheduler
+//
+//  Created by A.A Gde Yogi Pramana on 13/11/22.
+//
+
+#include <iostream>
+#include "Index.h"
+using namespace std;
+
+int main() {
+    
+    load();
+    login();
+    add_schedule();
+    matchSchedule();
+    
+    return 0;
+}
